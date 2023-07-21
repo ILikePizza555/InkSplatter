661ea70316d8804e3d1aad2257e0f910d2856e23
Izzy Lancaster <avrisaac555@gmail.com>
2023-07-20 18:07:54 -0700

    Added network connect functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Source code for the software running on my Inky Frame
+
+# External Libries
+
+These need to be installed with `mip` or `upip` or equivalent.
+
+- logging
--- src/useful.py
+++ src/useful.py
@@ -2,10 +2,17 @@ from pimoroni_i2c import PimoroniI2C
 from pcf85063a import PCF85063A
 from machine import Pin, PWM
 
+import logging
+import network
+import network_led
+import time
+
 HOLD_VSYS_EN_PIN = 2
 
 I2C_SDA_PIN = 4
 I2C_SCL_PIN = 5
+
+led_warn = Pin(6, Pin.OUT)
 
 # Pin setup for VSYS_HOLD needed to sleep and wake.
 hold_vsys_en_pin = None
@@ -21,3 +28,35 @@ def setup_real_time_chip():
         real_time_chip = PCF85063A(i2c)
 
 setup_real_time_chip()
+
+def network_connect(ssid, psk):
+    logging.debug("Enabling Wifi")
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active = True
+
+    # Number of attempts before timeout
+    max_wait = 10
+
+    logging.info("Connecting to %s", ssid)
+    network_led.pulse()
+    wlan.connect(ssid, psk)
+
+    while max_wait > 10:
+        status = wlan.status()
+        logging.debug("Connection attempt %d, status: %d", max_wait, status)
+
+        if status < 0 or status >= 3:
+            break
+
+        max_wait -= 1
+        time.sleep(1)
+    
+    network_led.stop()
+    network_led.pwm.duty_u16(30000)
+    status = wlan.status()
+
+    if status == 3:
+        logging.info("Connection successful.")
+    else:
+        logging.warning("Connection failed. Status: %d", status)
+        led_warn.on()
